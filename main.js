0bcaecf33cced09e05bd09464e9319e2ecb2dc4f
prem <prem.sherpa@codeimmersives.com>
2020-09-24 15:30:53 -0400

    api key

--- main.js
+++ main.js
@@ -6,7 +6,6 @@
 // Use any tools we've practiced such as promises, destructuring, etc.
 // Once everything works, upload the file and submit.
 
-
 // // Weather API- https://openweathermap.org/api
 // // Weather/Air Quality API-  https://www.airvisual.com/dashboard/api
 // // Recipe API- https://developer.edamam.com/ (uses two key code)
@@ -16,12 +15,49 @@
 // // News API - https://newsapi.org/
 // // Harvard Art Museum API - https://www.harvardartmuseums.org/collections/api
 
-
 // If you want to make your project a little more robust and dynamic you could use tools like the ones below:
 // Read the docs to see how it works. Not hard.
-
 
 // Readline: https://nodejs.org/api/readline.html
 // OR
 // Inquirer: https://www.npmjs.com/package/inquirer
 // Read the docs to find out how to use. Pretty intuitive.
+
+console.clear();
+const key = require('./.gitignore')
+const fetch = require("node-fetch");
+console.log(`${key}`)
+
+var url =
+  "http://newsapi.org/v2/top-headlines?" +
+  "country=us&" +
+  `apiKey=${key}`;
+//   var req = new Request(url);
+fetch(url).then((response) =>
+  // console.log(response.json());
+  response.json()
+).then((data)=>{
+    console.log(data.articles.slice(0,3));
+    
+    
+});
+
+// fetch(url)
+// .then((data) => data.json())
+
+// const request = require('request');
+// const argv = require('yargs').argv;
+
+// let apiKey = '*****************************';
+// let city = argv.c || 'portland';
+// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+// request(url, function (err, response, body) {
+//   if(err){
+//     console.log('error:', error);
+//   } else {
+//     let weather = JSON.parse(body)
+//     let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+//     console.log(message);
+//   }
+// });
