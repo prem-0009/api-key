2259d79fe53847386daa979c5468aff17c8357f0
prem <prem.sherpa@codeimmersives.com>
2020-09-28 10:24:11 -0400

    api keys

--- main.js
+++ main.js
@@ -25,11 +25,11 @@
 
 console.clear();
 const key = require('./key')
-// const key = "8c715e8a95cb4fe0abbbcf7463ecf1b5";
-const fetch = require("node-fetch");
-// console.log(`${key}`)
 
-let currentDate = new Date().toISOString().split("T")[0];
+const fetch = require("node-fetch");
+
+
+const currentDate = new Date().toISOString().split("T")[0];
 
 
 const readline = require("readline");
@@ -44,39 +44,29 @@ let display = `\nToday is ${currentDate} \n\nTo search by a category, type any o
 
 let cat = [  "business",  "entertainment",  "general",  "health",  "science",  "sports",  "technology",];
 
-
-
-
-
 let userInteraction = (userInput) => {//////////function.............
+  
   let category = "";
   let url = "";
   let word1 = '';
   
   category += cat.filter((item) => userInput === item)[0]; //since the filter return an array with one value ..it's in index 0 n only index..
-
-  // if (userInput !== category){}
-
   
   let urlCategory = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${key}`;
-  let urlByKeyWord =  `https://newsapi.org/v2/everything?q=${word1}&from=2020-09-25&to=${currentDate}&sortBy=popularity&apiKey=${key}` ;
-
+  // let urlByKeyWord =  `https://newsapi.org/v2/everything?q=${word1}&from=2020-09-25&to=${currentDate}&sortBy=popularity&apiKey=${key}` ;
 
   url = urlCategory;
   console.clear();
+
   if (userInput === "exit") {
     rl.close();
   }else{
-
-
-
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       let myData = data.articles;
-      
-      
+            
       if (userInput !== "exit" && userInput === category) {
         console.log(`\nThese are the ${category} news.`);
 
@@ -90,63 +80,33 @@ let userInteraction = (userInput) => {//////////function.............
 
         rl.question(display, userInteraction);
       }
-      if (userInput !== category){
-        return myData;
-      }
-
-      
     })
-
-    // .then((myData)=>{
-    //   if (userInput !== category){
-    //     word1 = userInput;
-    //     url =  `https://newsapi.org/v2/everything?q=${word1}&from=2020-09-25&to=${currentDate}&sortBy=popularity&apiKey=${key}` ;
-
-    //     myData.forEach(({ source: { name }, author, description: desc, content, url }, i) => {
-    //       if (i <= 2 && desc) {
-    //         console.log(`\n\n${name}\nauthor :${author}\n${content}\n\n`);
-    //         console.log("----------------------------------------------------");
-    //       }
-    //     }
-    //   );
-
-    //   rl.question(display, userInteraction);
-    //   }
-      
-    // });
-
   }
 };
 
-// rl.question(`If you want all "Technology" news type: hi there?\n`,(answer) => {
-//     // to read the word typed into command line
-//     // TODO: Log the answer in a database
-//     console.log(`Thank you for your valuable feedback: ${answer}`);
-//     rl.close();
-//   }
-// );
-// rl.on("close", () => {
-//   console.log("correct answer");
+
+// rl.question(display, userInteraction);
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   prompt: 'OHAI> '
 // });
 
-rl.question(display, userInteraction);
+// rl.prompt();
 
-// fetch(url)
-// .then((data) => data.json())
-
-// const request = require('request');
-// const argv = require('yargs').argv;
-
-// let apiKey = '*****************************';
-// let city = argv.c || 'portland';
-// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-// request(url, function (err, response, body) {
-//   if(err){
-//     console.log('error:', error);
-//   } else {
-//     let weather = JSON.parse(body)
-//     let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-//     console.log(message);
+// rl.on('line', (line) => {
+//   switch (line.trim()) {
+//     case 'hello':
+//       console.log('world!');
+//       break;
+//     default:
+//       console.log(`Say what? I might have heard '${line.trim()}'`);
+//       break;
 //   }
+//   rl.prompt();
+// }).on('close', () => {
+//   console.log('Have a great day!');
+//   process.exit(0);
 // });
